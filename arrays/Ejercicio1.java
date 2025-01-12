2fc6ceb232fdf6870f22525c676fbf9d2b4efe91
samuu2200 <samuel2200@hotmail.es>
2025-01-12 21:35:55 +0100

    Ejercicio 1 de arrays en casa

--- arrays/Ejercicio1.java
+++ arrays/Ejercicio1.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Crear un array de 5 números enteros
+        int[] numeros = new int[5];
         
-        System.out.println("Hola que tal");
-        System.out.println("Quiero que aparezcas en GitHub");
-        
+        //Solicitar los valores al usuario y asignarlos al array
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Introduce el valor para la posición: ");
+            numeros[i] = scan.nextInt();
+        }
+
+        //Imprimir cada valor del array en una línea nueva
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+
+        scan.close();
     }
 
 }
