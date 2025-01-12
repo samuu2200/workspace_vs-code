d28898b6e46b323668350e3ff4ac0edeb268d8f3
samuu2200 <samuel2200@hotmail.es>
2025-01-12 19:58:59 +0100

    Prueba github

--- arrays/Ejercicio1.java
+++ arrays/Ejercicio1.java
@@ -3,6 +3,7 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         
         System.out.println("Hola que tal");
+        System.out.println("Quiero que aparezcas en GitHub");
         
     }
 
